4db7047edb1c0a365a6258927c69c451032d2f1c
pedro <pedrohensimao2@gmail.com>
2025-10-15 15:17:43 +0100

    Docker Container created and database connection made.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from models.daily_diet_db import db
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:admin123@127.0.0.1:3306/daily-diet"
+db.init_app(app)
 
 @app.route("/")
 def hello_world():
---
+++ models/daily_diet_db.py
@@ -0,0 +1,2 @@
+from flask_sqlalchemy import SQLAlchemy #conexao com bd
+db = SQLAlchemy()  #conexao da bd com a app
