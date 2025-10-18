b7637657de01da4d4714df4b2c546b760d0091ba
pedro <pedrohensimao2@gmail.com>
2025-10-18 16:57:01 +0100

    new table Users created for future auth system

---
+++ models/users.py
@@ -0,0 +1,7 @@
+from models.daily_diet_db import db
+
+class Users(db.Model):
+    #id, username and password
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), nullable= False, unique = True)
+    password = db.Column(db.String(80), nullable= False)
