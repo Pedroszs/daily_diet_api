36849ae4b6f10d00c7d5e5a8be76f3fbaa2b940e
pedro <pedrohensimao2@gmail.com>
2025-10-18 16:58:32 +0100

    Function to Edit the past register implemented.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from models.daily_diet_db import db
 from models.diet import Diet
+from models.users import Users
 from datetime import datetime
+
+#CRUD: Create(POST), Read(GET), Upd(PUT), Del(DELETE)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -31,6 +34,22 @@ def new_register():
         return jsonify({"message": "Refeição registrada com sucesso!!"})
     
     return jsonify({"message": "Verifique se foram adicionadas todas as informações necessárias."}), 404
+
+@app.route("/diet/<int:id_diet>", methods=["PUT"])
+def upd_register(id_diet):
+    data = request.json
+    diet = Diet.query.get(id_diet)
+
+    if diet:
+        diet.name = data.get("name")
+        diet.description = data.get("description")
+        diet.date_time = data.get("date_time")
+        diet.f_diet = data.get("f_diet")
+        db.session.commit()
+        return jsonify({"message": f"alterações no registro {id_diet} realizadas com sucesso"})
+    
+    return jsonify({"messagem": "A tarefa selecionada não existe"}), 400
+
 
 
 
