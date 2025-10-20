b5e35eb9d9e270a1734272cffb8a2446c3940e9e
pedro <pedrohensimao2@gmail.com>
2025-10-20 17:27:43 +0100

    User authentication functions implemented: Register new user/ login user/ load currently user.

--- app.py
+++ app.py
@@ -1,15 +1,65 @@
 from flask import Flask, request, jsonify
 from models.daily_diet_db import db
 from models.diet import Diet
-from models.users import Users
+from models.users import User
 from datetime import datetime
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 #CRUD: Create(POST), Read(GET), Upd(PUT), Del(DELETE)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:admin123@127.0.0.1:3306/daily-diet-db"
+
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+# Showing with http route im using for the login
+login_manager.login_view = "login" 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        #login
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "autenticação realizada com sucesso"})
+    
+    return jsonify({"message": "credenciais invalidos"}), 400
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"})
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role="user") 
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuario cadastrado com sucesso!"})
+
+    return jsonify({"message": "Dados invalidos."}), 400
+
 
 @app.route("/diet", methods=["POST"])
 def new_register():
@@ -60,6 +110,8 @@ def del_register(id_diet):
         return jsonify ({"message": f"Registro {id_diet} deletado com sucesso!!"})
     
     return jsonify({"message": "O registro insediro não existe"}), 400
+
+
 
 
 
--- models/users.py
+++ models/users.py
@@ -1,7 +1,8 @@
 from models.daily_diet_db import db
 
-class Users(db.Model):
+class User(db.Model):
     #id, username and password
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), nullable= False, unique = True)
     password = db.Column(db.String(80), nullable= False)
+    role =  db.Column(db.String(80), nullable = False, default ="user")
