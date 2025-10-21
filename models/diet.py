989fdf0b526fe1aad52d538c52c1876de4c47dc6
pedro <pedrohensimao2@gmail.com>
2025-10-21 18:06:48 +0100

    Redesigned the databases and created a foreignkey to create a realation between the user tables and the diets tables, making possible to list the loged user diets

--- models/diet.py
+++ models/diet.py
@@ -2,10 +2,15 @@ from models.daily_diet_db import db
 from datetime import datetime
 
 class Diet(db.Model):
+    __tablename__ = "diets"
     #id, name, description, date-time, following the diet?
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80), nullable = False)
     description = db.Column(db.String(180), nullable = True, default="")
     date_time = db.Column(db.DateTime(), nullable = False, default=datetime.utcnow)
     f_diet = db.Column(db.Boolean(), nullable = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", back_populates="diets")
+    #foreignKey, chave estrangeira utilizada para referenciar tabelas
+
 
--- models/users.py
+++ models/users.py
@@ -1,8 +1,12 @@
 from models.daily_diet_db import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
     #id, username and password
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), nullable= False, unique = True)
     password = db.Column(db.String(80), nullable= False)
     role =  db.Column(db.String(80), nullable = False, default ="user")
+    diets = db.relationship("Diet", back_populates="user", lazy=True)
+    #relationship, criar relações entre as tabelas.
